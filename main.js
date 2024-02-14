7e274aa26ced2339257fabb016782878e185b0ad
Orangejuice424 <132031252+Orangejuice424@users.noreply.github.com>
2024-02-13 18:46:45 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+ noseX=0
+ noseY=0
+
+
+function preload(){ 
+ }    
+
+function setup(){
+mycanvas=createCanvas(600,450)
+mycanvas.center()
+Video=createCapture(VIDEO)
+Video.hide()
+
+pn=ml5.poseNet(Video, ModelLoaded)
+    pn.on('pose', gotResult)
+
+}
+
+function draw(){
+    image(Video,0, 0, 600, 450)
+}
+
+function ModelLoaded(){
+    console.log('Model has loaded')
+
+}
+
+function gotResult(result){
+    if(result.length>0){
+        console.log(result)
+        noseX=result[0].pose.nose.x
+        noseY=result[0].pose.nose.y
+
+        }
+    }
